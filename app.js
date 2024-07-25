19c94848ce1f2a42a42d40e2bbc8c6ebffc1b86c
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-25 09:35:00 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const { ClerkExpressRequireAuth } = require ('@clerk/clerk-sdk-node')
+const express  = require ('express');
+const cors = require('cors'); // Importando o módulo cors
+
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3003;
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const admin = require('./routes/admin');
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+// Configurações e middlewares
+app.use(cors({ origin: "*"}));
+// Use the strict middleware that raises an error when unauthenticated
+app.get(
+  '/protected-endpoint',
+  ClerkExpressRequireAuth({
+    // Add options here
+    // See the Middleware options section for more details
+  }),
+  (req, res) => {
+    res.send('vc esta autorisado')
+    res.json(req.auth);
+  }
+);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(401).send('Unauthenticated!');
+});
+
+app.use('/api', admin);
+
+// Acesso à variável de ambiente MONGODB_URI do arquivo .env
+const uri = process.env.MONGODB_URI;
+
+
+// Conexão com o banco de dados
+mongoose.connect(uri, {
+ 
+}).then(() => {
+  console.log('Conectado ao banco de dados');
+}).catch((error) => {
+  console.error('Erro de conexão com o banco de dados:', error);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
