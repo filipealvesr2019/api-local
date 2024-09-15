9220e1f39cee23b9a48dbd534a7437dd23426f6b
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-09-15 16:06:23 -0300

    refactor: refaz o build

--- api/index.js
+++ api/index.js
@@ -1,6 +1,3 @@
-import express from 'express';
-import app from '../app'; // Import your main app
+const app  =  require ("../app");
 
-const handler = (req, res) => app(req, res);
-
-export default handler;
+export default app;
